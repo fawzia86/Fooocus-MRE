dfd65fb3393f1c1e643387404a17c741eba890ea
MoonRide303 <moonride303@gmail.com>
2023-08-24 19:57:49 +0200

    Added downloaders for Revision and Control-LoRA model files

--- modules/path.py
+++ modules/path.py
@@ -7,6 +7,8 @@ from os.path import exists
 def load_paths():
     path_checkpoints = '../models/checkpoints/'
     path_loras = '../models/loras/'
+    path_clip_vision = '../models/clip_vision/'
+    path_controlnet = '../models/controlnet/'
     path_outputs = '../outputs/'
 
     if exists('paths.json'):
@@ -17,20 +19,27 @@ def load_paths():
                     path_checkpoints = paths_obj['path_checkpoints']
                 if 'path_loras' in paths_obj:
                     path_loras = paths_obj['path_loras']
+                if 'clip_vision' in paths_obj:
+                    path_clip_vision = paths_obj['clip_vision']
+                if 'controlnet' in paths_obj:
+                    path_controlnet = paths_obj['controlnet']
                 if 'path_outputs' in paths_obj:
                     path_outputs = paths_obj['path_outputs']
+
             except Exception as e:
                 print(e)
             finally:
                 paths_file.close()
 
-    return path_checkpoints, path_loras, path_outputs
+    return path_checkpoints, path_loras, path_clip_vision, path_controlnet, path_outputs
 
 
-path_checkpoints, path_loras, path_outputs = load_paths()
+path_checkpoints, path_loras, path_clip_vision, path_controlnet, path_outputs = load_paths()
 
 modelfile_path = path_checkpoints if os.path.isabs(path_checkpoints) else os.path.abspath(os.path.join(os.path.dirname(__file__), path_checkpoints))
 lorafile_path = path_loras if os.path.isabs(path_loras) else os.path.abspath(os.path.join(os.path.dirname(__file__), path_loras))
+clip_vision_path = path_clip_vision if os.path.isabs(path_loras) else os.path.abspath(os.path.join(os.path.dirname(__file__), path_clip_vision))
+controlnet_path = path_loras if os.path.isabs(path_controlnet) else os.path.abspath(os.path.join(os.path.dirname(__file__), path_controlnet))
 temp_outputs_path = path_outputs if os.path.isabs(path_outputs) else os.path.abspath(os.path.join(os.path.dirname(__file__), path_outputs))
 
 os.makedirs(temp_outputs_path, exist_ok=True)
@@ -38,6 +47,8 @@ os.makedirs(temp_outputs_path, exist_ok=True)
 default_base_model_name = 'sd_xl_base_1.0_0.9vae.safetensors'
 default_refiner_model_name = 'sd_xl_refiner_1.0_0.9vae.safetensors'
 default_lora_name = 'sd_xl_offset_example-lora_1.0.safetensors'
+default_clip_vision_name = 'clip_vision_g.safetensors'
+default_controlnet_name = 'control-lora-canny-rank256.safetensors'
 default_lora_weight = 0.5
 
 model_filenames = []
