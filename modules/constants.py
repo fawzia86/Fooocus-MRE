74e7ee8936f395c80ddfa5bf81d224f3b408a5c4
MoonRide303 <moonride303@gmail.com>
2023-09-10 21:48:34 +0200

    Adjusted seed limits to those in transformers (trainer_utils.py)

--- modules/constants.py
+++ modules/constants.py
@@ -3,5 +3,6 @@ STEPS_QUALITY = 60
 SWITCH_SPEED = 20
 SWITCH_QUALITY = 40
 
+# limited by modules\expansion.py -> transformers\trainer_utils.py
 MIN_SEED = 0
-MAX_SEED = 2**63 - 1
+MAX_SEED = 2**32 - 1
